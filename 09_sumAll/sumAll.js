715b2dfc1561a4aaa3e4bc7288e84b8149792b92
mCayC <m_celik2001@live.nl>
2025-06-22 15:11:50 +0200

    exercise 9 and 10 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    //guard 1: must be integer
+    if(!Number.isInteger(lowerBound) || !Number.isInteger(upperBound)){
+        return "ERROR";
+    }
+    //guard 2: negative number
+    if(lowerBound < 0 || upperBound < 0){
+        return "ERROR";
+    }
 
+    //Make sure the lowerBound is the lower value
+    if(lowerBound > upperBound){
+        let temp = upperBound;
+        upperBound = lowerBound;
+        lowerBound = temp;
+    }
+    let result = 0;
+    for(let x=lowerBound; x <= upperBound; x++){
+        result += x;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //Guard invalid input
+    if(!Number.isInteger(year)){
+        return "ERROR";
+    }
 
+    if(year % 4 == 0){
+        if(year % 100 != 0 || year % 400 == 0){
+            return true;
+        }
+    }
+    //default return
+    return false;
 };
 
 // Do not edit below this line
