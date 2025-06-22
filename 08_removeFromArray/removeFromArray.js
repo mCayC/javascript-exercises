ae802a1724e138f19d04d3afa02084595c0fc75b
mCayC <m_celik2001@live.nl>
2025-06-22 14:58:16 +0200

    exercise 8 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(set, ...filterOut) {
+    if(filterOut == null){return set;}
+    return set.filter(x => !filterOut.includes(x));
 };
 
 // Do not edit below this line
