490bafc9add758aa90dc6723714c5c2e09c114a8
mCayC <m_celik2001@live.nl>
2025-06-22 14:51:10 +0200

    javascript exercise 6 and 7 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, n) {
+    if(n < 0){
+        return "ERROR";
+    }
+    let res = "";
+    for(let x = 0; x < n; x++){
+        res += text;
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let partitions = word.split('');
+    let result = "";
+    for(let x = partitions.length-1; x>=0;x--){
+        result += partitions[x];
+    }
+    return result;
 };
 
 // Do not edit below this line
